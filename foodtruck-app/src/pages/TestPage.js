a9ac76c7e60283b9f108f7c3826967be55719b8a
Alex <alexcouts18@gmail.com>
2020-03-27 19:24:05 -0400

    completed styling for truck pages based on dummy data

--- foodtruck-app/src/pages/TestPage.js
+++ foodtruck-app/src/pages/TestPage.js
@@ -22,33 +22,36 @@ const TestPage =  (props) => {
         )
     }, [])
     console.log(selectedData)
+  
   return (
-    <div>
-    <h1 className="profile-title"> {selectedData.truckName}</h1>
-    <div className="profile-container">
-    
-    <img className="profile-item" src={selectedData.imageOfTruck}/>
-    <div className="profile-item" style={{ margin: "0 auto", width: 800 }} >
-      <Map
-        google={props.google}
-        zoom={14}
-        style={mapStyles}
-        center={{
-          lat: location.lat,
-          lng: location.lng
+    <div className="truck-page-container">
+    <div className="truck-page-container__profile-container">
+        <img className="profile-container__truck-image" src={selectedData.imageOfTruck}/>
+        <h1 className="profile-container__truck-name"> {selectedData.truckName}</h1>
+        <div className="profile-container__location-container">
+          <h2 className="profile-info">Address: {selectedData.location}</h2>
+          <h2 className="profile-info">Cusine: {selectedData.cusineType}</h2>
+      </div>  
+    </div>
+    <div className="truck-page-container__truck-map"> {/* style={{ margin: "0 auto", width: 800 }} */}
+        <Map
+          google={props.google}
+          zoom={14}
+          style={mapStyles}
+          center={{
+            lat: location.lat,
+            lng: location.lng
         }}
-      />
+        />
     </div>
-    </div>
-      <h2 className="profile-info">Address: {selectedData.location}</h2>
-      <h2 className="profile-info">Cusine: {selectedData.cusineType}</h2>
     </div>
   );
 };
 
+// style="position: absolute; width: 30%; height: 50%;"
 const mapStyles = {
-  width: 800,
-  height: 800
+  width: 647,
+  height: 850
 };
 
 export default GoogleApiWrapper({
